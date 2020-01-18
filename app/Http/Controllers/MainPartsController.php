6372b5406da0d5b32d95fefe9fef0440d15533bd
IjdiyaouJaafar <ijdiyaoujaafar@gmail.com>
2020-01-18 14:01:44 +0100

    just finished the re-organization of the mainpart & make a unique entrance

--- app/Http/Controllers/MainPartsController.php
+++ app/Http/Controllers/MainPartsController.php
@@ -2,12 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Chapitre;
-use App\Filiere;
-use App\Module;
+
 use App\Niveau;
-use App\Semestre;
-use App\semestreModule;
+
 
 use Illuminate\Http\Request;
 
@@ -21,121 +18,14 @@ class MainPartsController extends Controller
 
     }
 
-    private $niveaux;
-    private $filieres;
-    private $modules;
-    private $allAssocSemestreModules;
-    private $chapitres;
-
 
     public function create()
     {
-        $this->niveaux = Niveau::all();
-        $this->filieres = Filiere::all();
-        $this->modules = Module::all();
-        $this->allAssocSemestreModules = semestreModule::all();
-        $this->chapitres = Chapitre::all();
+
         return view(
-            'mainparts.create',
-            ['modules' => $this->modules, 'filieres' => $this->filieres, 'niveaux' => $this->niveaux, 'assocSemestreModule' => $this->allAssocSemestreModules, 'chapitres' => $this->chapitres]
+            'mainparts.create'
         );
     }
 
 
-
-
-    //Création d'un nouveau chapitre
-
-    public function createChapitre(Request $request)
-    {
-        $chapitre = new Chapitre();
-        $chapitre->nom_chapitre = mb_strtoupper($request->input('nom_chapitre'));
-        $selectModule = $request->input('moduleChapitre');
-        $moduleExistant = Module::get()->where('nom_module', mb_strtoupper($selectModule))->first();
-        $chapitresExistant = Chapitre::get()->where('module_id', $moduleExistant->id);
-        if ($chapitresExistant) {
-            foreach ($chapitresExistant as $chapitreItem) {
-                if ($chapitreItem->nom_chapitre == $chapitre->nom_chapitre) {
-                    $messagePane = 'Ce Chapitre est déjà associé à ce module';
-                    return $messagePane;
-                }
-            }
-            $chapitre->module()->associate($moduleExistant)->save();
-            $messagePane = 'Ce Chapitre a été associé au module';
-            return $messagePane;
-        } else {
-            $chapitre->module()->associate($moduleExistant)->save();
-            $messagePane = 'Le nouveau chapitre a été associé au module';
-            return $messagePane;
-        }
-    }
-
-
-    public function updateChapitre(Request $request, $idChapitre)
-    {
-        $chapitreExistant = Chapitre::findOrFail($idChapitre);
-        if ($chapitreExistant) {
-            if ($chapitreExistant->nom_chapitre == mb_strtoupper($request->get('chapitre'))) {
-                $chapitreExistant->nom_chapitre = mb_strtoupper($request->get('chapitre'));
-                $chapitreExistant->save();
-                return 1;
-            } else {
-                $chapitreExistant1 = Chapitre::get()->where('nom_module', mb_strtoupper($request->get('chapitre')))->first();
-                if ($chapitreExistant1) {
-                    return -1;
-                } else {
-                    $chapitreExistant->nom_chapitre = mb_strtoupper($request->get('chapitre'));
-                    $chapitreExistant->save();
-                    return 2;
-                }
-            }
-        } else {
-            return -2;
-        }
-    }
-
-
-    public function deleteChapitre($idChapitre)
-    {
-        $chapitreExistant = Chapitre::findOrFail($idChapitre);
-        if ($chapitreExistant) {
-            Chapitre::destroy($idChapitre);
-            return 1;
-        } else {
-            return -2;
-        }
-    }
-
-
-    //Génération du select module par la filiere
-
-    public function modulesFiliere(Request $request)
-    {
-        $this->validate($request, ['nom_filiere' => 'required|exists:filieres,nom_filiere']);
-        $filiereExistant = Filiere::get()->where('nom_filiere', mb_strtoupper($request->get('nom_filiere')))->first();
-        $semestresFiliere = Semestre::get()->where('filiere_id', $filiereExistant->id);
-        $data = array();
-        foreach ($semestresFiliere as $semestre) {
-            $array_semestre_module = semestreModule::get()->where('semestre_id', $semestre->id);
-            foreach ($array_semestre_module as $semestre_module) {
-                $moduleExistant = Module::get()->where('id', $semestre_module->module_id)->first();
-                array_push($data, $moduleExistant);
-            }
-        }
-        $modulesData['data'] = $data;
-        return json_encode($modulesData);
-    }
-
-    public function semestresFiliere(Request $request)
-    {
-        $this->validate($request, ['nom_filiere' => 'required|exists:filieres,nom_filiere']);
-        $filiereExistant = Filiere::get()->where('nom_filiere', mb_strtoupper($request->get('nom_filiere')))->first();
-        $semestresFiliere = Semestre::get()->where('filiere_id', $filiereExistant->id);
-        $data = array();
-        foreach ($semestresFiliere as $semestre) {
-            array_push($data, $semestre);
-        }
-        $semestresData['data'] = $data;
-        return json_encode($semestresData);
-    }
 }
--- resources/views/mainparts/create.blade.php
+++ resources/views/mainparts/create.blade.php
@@ -2,16 +2,23 @@
 
 @section('mainContent')
 
-    <div class="row z-depth-4 mt-p-1">
-        <div class="col s12">
-            <ul class="tabs deep-orange-text text-accent-3">
-                <li class="tab col s3"><a href="{{route('mainParts.niveau')}}">Niveau</a></li>
-                <li class="tab col s3"><a href="{{route('mainParts.filiere')}}">Filière</a></li>
-                <li class="tab col s3"><a href="{{route('mainParts.module')}}">Module</a></li>
-                <li class="tab col s3"><a href="{{route('mainParts.chapitre')}}">Chapitre</a></li>
-            </ul>
-        </div>
+    <div class="row z-depth-4 mt-p-1" style="padding-top: 12%">
+        <span style="padding-left: 39.7%;font-size: x-large;padding-bottom: 5%">Plan pédagogique</span>
+        <br><br><br>
+        <nav style="background-color: white">
+            <div class="nav-wrapper col s12">
+                <ul id="nav-mobile" class="center hide-on-med-and-down " style="background-color: white">
+                    <li class="col s3"><a style="color: orangered" href="{{route('mainParts.niveau')}}">NIVEAU</a></li>
+                    <li class="col s3"><a style="color: orangered" href="{{route('mainParts.filiere')}}">FILIÈRE</a>
+                    </li>
+                    <li class="col s3"><a style="color: orangered" href="{{route('mainParts.module')}}">MODULE</a></li>
+                    <li class="col s3"><a style="color: orangered" href="{{route('mainParts.chapitre')}}">CHAPITRE</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </div>
+
 @endsection
 
 
